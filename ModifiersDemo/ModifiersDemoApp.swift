4b39f159b35583905c4ead1a0dc382d0b3ba55eb
Davide Troise <davide@troise.net>
2022-08-28 18:51:43 +0200

    Initial Commit

---
+++ ModifiersDemo/ModifiersDemoApp.swift
@@ -0,0 +1,18 @@
+//
+//  ModifiersDemoApp.swift
+//  ModifiersDemo
+//
+//  Created by Davide Troise with Swift 5.0
+//  for the YouTube channel "Coding con Davide" https://bit.ly/3QJziJE
+//
+
+import SwiftUI
+
+@main
+struct ModifiersDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
